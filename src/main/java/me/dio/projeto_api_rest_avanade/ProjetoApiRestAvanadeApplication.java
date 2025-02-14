7a274cd80ae4da8be5e1a90e3ca2b315af316069
felipe-luis <felipellopes12@hotmail.com>
2025-02-14 14:45:42 -0300

    Initial commit

---
+++ src/main/java/me/dio/projeto_api_rest_avanade/ProjetoApiRestAvanadeApplication.java
@@ -0,0 +1,13 @@
+package me.dio.projeto_api_rest_avanade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiRestAvanadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiRestAvanadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/projeto_api_rest_avanade/ProjetoApiRestAvanadeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.projeto_api_rest_avanade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApiRestAvanadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
